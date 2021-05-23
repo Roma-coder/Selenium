e9d6da230fc4f377f1dc2830be78138fcaa7c16d
Roma-coder <56150485+Roma-coder@users.noreply.github.com>
2021-05-23 16:36:06 +0300

    add tests

---
+++ loginTest1.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+wd = webdriver.Chrome(chrome_options=options)
+
+wd.get('http://localhost:3000/login')
+emailElement = wd.find_element_by_css_selector("#login_email_field")
+emailElement.clear()
+emailElement.send_keys('romaIv17221@gmail.com')
+
+passElement = wd.find_element_by_css_selector("#login_password_field")
+passElement.clear()
+passElement.send_keys('123456')
+
+btn_element = wd.find_element_by_css_selector("#login_button")
+btn_element.click()
+
+time.sleep(2)
+
+assert wd.current_url == 'http://localhost:3000/room'
---
+++ registerTest2.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import time
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+    
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+wd = webdriver.Chrome(chrome_options=options)
+
+wd.get('http://localhost:3000/register')
+emailElement = wd.find_element_by_css_selector("#register_username_field")
+emailElement.clear()
+emailElement.send_keys(get_random_string(7))
+
+passElement = wd.find_element_by_css_selector("#register_email_field")
+passElement.clear()
+passElement.send_keys('romaIv172@gmail.com')
+
+passElement = wd.find_element_by_css_selector("#register_password_field")
+passElement.clear()
+passElement.send_keys('123456')
+
+passElement = wd.find_element_by_css_selector("#register_re_password_field")
+passElement.clear()
+passElement.send_keys('123456')
+
+btn_element = wd.find_element_by_css_selector("#register_button")
+btn_element.click()
+
+time.sleep(5)
+
+message_element = wd.find_element_by_css_selector(".alert-success")
+
+
+assert message_element != None
